01107b51c787c0d13c455e95f61bdcfc99ca0265
DeeaMG <55800409+DeeaMG@users.noreply.github.com>
2022-03-15 01:09:43 +0200

    Update App.js
    Changed from class component to functional component.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,56 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component.js";
 import SearchBox from "./components/search-box/search-box.component.js";
 // import data from "./external/monsters.js";
 import data from "./external/weapons.json";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  console.log("render");
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  console.log(searchField);
 
-  componentDidMount() {
-    /* monsters.js
-    this.setState(() => {
-      return { monsters: data };
-    });
-    */
+  /*useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);*/
 
-    // monsters.json - ?Says "Compiled with problems", but it takes json correctly from the file.
-    this.setState(() => {
-      return { monsters: data.map((user) => user) };
-    });
+  /* monsters.js
+  useEffect(() => {
+    setMonsters(data);
+  }, []);*/
 
-    // fetch("https://jsonplaceholder.typicode.com/users")
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((users) =>
-    //     this.setState(() => {
-    //       return { monsters: users };
-    //     })
-    //   );
-  }
+  useEffect(() => {
+    setMonsters(data.map((user) => user));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  // Otherwise the filteredMonsters get's refiltered whenever an event is triggerd, not only when monsters or searchField change.
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Metin2 Dex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder={"Search monster"}
-          className={"monsters-search-box"}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  const onSearchChange = (event) => {
+    const searchFieldStr = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldStr);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Metin2 Dex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder={"Search monster"}
+        className={"monsters-search-box"}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
